1f1475832b4440935cb26a542224e7107f4198ee
Steve Lambe <firrae@gmail.com>
2015-12-04 22:23:13 -0500

    Bumped version and added some version constraints.

--- package.js
+++ package.js
@@ -2,18 +2,20 @@
 Package.describe({
   name: 'firrae:react-mediumeditor',
   summary: 'MediumEditor wrapped up in a ReactJS component.',
-  version: '0.1.1',
+  version: '0.1.0',
   git: 'https://github.com/firrae/react-mediumeditor',
   documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
+  api.versionsFrom('METEOR@1.2.0');
+
   api.use([
     "ecmascript",
     "less",
     "jquery",
     "mediumeditor:mediumeditor",
-    "react"
+    "react@0.1.13'"
   ], ['client','server']);
 
   api.addFiles([
